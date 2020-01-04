f72a153be992a184b5db5e9f6e92f03d93b7003a
bbosuni1 <bbosuni1@naver.com>
2020-01-04 22:35:58 +0900

    2020/01/04 Commit

--- src/main/java/com/kh/fifteenGG/search/controller/SummonerSearch.java
+++ src/main/java/com/kh/fifteenGG/search/controller/SummonerSearch.java
@@ -1,8 +1,8 @@
 package com.kh.fifteenGG.search.controller;
 
+import com.merakianalytics.orianna.Orianna;
 import com.merakianalytics.orianna.types.common.Region;
-import com.merakianalytics.orianna.types.core.staticdata.Champion;
-import com.merakianalytics.orianna.types.core.staticdata.Champions;
+import com.merakianalytics.orianna.types.core.summoner.Summoner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +14,13 @@ public class SummonerSearch {
     @RequestMapping("/search/SummonerSearch.do")
     public String SummonerSearch(@RequestParam String summonerName, Model model) {
 
+        Orianna.loadConfiguration("config.json");
+        Orianna.setDefaultRegion(Region.KOREA);
+        Orianna.setRiotAPIKey("RGAPI-f60cb18e-0c5c-463f-af23-f57d0b8b96e4");
 
+        Summoner summoner = Orianna.summonerNamed(summonerName).get();
+
+        System.out.println(summoner);
 
         return "summoner/summonerView";
 
