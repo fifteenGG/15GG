7f899920dd17fd3281112e9a524a7b176683706a
bbosuni1 <bbosuni1@naver.com>
2020-01-06 16:59:46 +0900

    2020/01/06 Commit

---
+++ src/main/java/com/kh/fifteenGG/search/controller/model/service/SearchService.java
@@ -0,0 +1,14 @@
+package com.kh.fifteenGG.search.controller.model.service;
+
+import com.kh.fifteenGG.search.controller.model.vo.match.Match;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SearchService {
+
+    public int insertMatch(Match match) {
+        int result = 0;
+
+        return result;
+    }
+}
