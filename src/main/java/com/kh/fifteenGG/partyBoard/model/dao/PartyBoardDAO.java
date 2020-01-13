8dfd6ef483318bffd25ab1c3d28efec314429c97
bbosuni1 <bbosuni1@naver.com>
2020-01-13 12:14:07 +0900

    2020/01/13 commit 회원가입 수정 + 챔피언 tip Page + 파티 찾기 게시판

---
+++ src/main/java/com/kh/fifteenGG/partyBoard/model/dao/PartyBoardDAO.java
@@ -0,0 +1,43 @@
+package com.kh.fifteenGG.partyBoard.model.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.fifteenGG.partyBoard.model.vo.PartyBoard;
+
+@Repository
+public class PartyBoardDAO {
+	
+	@Autowired
+	SqlSessionTemplate sqlSession;
+
+	public int insertParty(PartyBoard partyBoard) {
+		return sqlSession.insert("partyBoard-mapper.insertParty", partyBoard);
+	}
+
+	
+	public List<PartyBoard> partyBoardList(PartyBoard partyBoard) { 
+		return sqlSession.selectList("partyBoard-mapper.selectListParty", partyBoard); 
+		}
+	 
+
+	public int totalCountContent() {
+		return sqlSession.selectOne("partyBoard-mapper.totalCountContent");
+	}
+
+
+	public List<PartyBoard> searchList(PartyBoard partyBoard) {
+		return sqlSession.selectList("partyBoard-mapper.searchList", partyBoard);
+	}
+
+
+
+	
+	
+	
+
+}
